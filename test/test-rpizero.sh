b16077cd2a06f64a8ebfb5047818e7d432c9d294
Daniel Anderson <804021+dananderson@users.noreply.github.com>
2021-01-28 12:18:18 -0800

    Clean up profile specific gcc flags. Add configuration of flags through env variables. Move build to github actions.

--- test/test-rpizero.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-export PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )/../bin:$PATH"
-
-rm -rf node_modules package-lock.json yarn.lock
-
-cross rpizero yarn --force
